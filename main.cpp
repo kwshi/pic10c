7c412408882570aeea8f0b4942a837ccc4845e66
Kye Shi <kiwishi@pm.me>
2024-04-16 15:31:48 -0700

    personalize player names

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
-#include <iostream>
 #include "rps.cpp"
+#include <iostream>
 
 int main() {
   char p1, p2;
-  std::cout << "player 1 plays [rps]: ";
+  std::cout << "jackie plays [rps]: ";
   std::cin >> p1;
 
-  std::cout << "player 2 plays [rps]: ";
+  std::cout << "chris plays [rps]: ";
   std::cin >> p2;
 
   int result{rps_who_wins(p1, p2)};
   switch (result) {
-    case 0:
-      std::cout << "tie" << std::endl; break;
-    case 1:
-      std::cout << "player 1 wins" << std::endl; break;
-    case 2:
-      std::cout << "player 2 wins" << std::endl; break;
+  case 0:
+    std::cout << "tie" << std::endl;
+    break;
+  case 1:
+    std::cout << "jackie wins" << std::endl;
+    break;
+  case 2:
+    std::cout << "chris wins" << std::endl;
+    break;
   }
-
 }
